a79c2660c02186f63dc307bbe37c709605e80ba1
Felectro9 <sramerafe1@gmail.com>
2022-09-08 06:22:12 -0700

    commiting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: c program using put function
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+:w
+q
+
