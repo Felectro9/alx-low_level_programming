3ed7eac85915d135509fb7b6c62855399bd88895
Felectro9 <sramerafe1@gmail.com>
2022-09-10 13:33:51 -0700

    prints alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - print alphabets, lower case and upper case
+ *
+ * return 0 (Always/Success)
+ */
+
+int main(void)
+{
+	char alp = 'a';
+        char ALP = 'A';
+	
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		++alp;
+	}
+
+
+	while (ALP <= 'Z')
+	{
+		putchar(ALP);
+		++ALP;
+	}
+	putchar('\n')
+
+	return (0)
+}
