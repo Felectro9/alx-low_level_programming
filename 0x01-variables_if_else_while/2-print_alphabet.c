8f1d55a1a22bb3a0dd8b1fe7dd9864f44bab26b7
Felectro9 <sramerafe1@gmail.com>
2022-09-10 12:46:25 -0700

    editing code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - print lower case alphabest
+ *
+ * return 0 (success)
+ * 
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= z)
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
