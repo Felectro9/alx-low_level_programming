607c0dea8e22589f76e46360a3226d20ba71d61f
Felectro9 <sramerafe1@gmail.com>
2022-09-10 14:27:35 -0700

    lower case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: print all aplhabet letters except q and e
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *      */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q')
+		continue;
+
+		else if (ch == 'e')
+		continue;
+	
+	putchar(ch);
+	}
+}
