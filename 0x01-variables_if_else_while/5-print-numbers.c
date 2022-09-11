13e9bf6bb612f50c7097a7468fd778f7251dd75c
Felectro9 <sramerafe1@gmail.com>
2022-09-10 22:34:39 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print-numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *
+ * main -Entry point
+ *
+ * description - prints numbers between 0 and 10
+ *
+ * return (0) (always/success)
+ */
+
+int main(void)
+{
+	int n;
+	n = 0;
+	while(n <= 9)
+	{
+		printf("%i", n)
+			++n;
+
+	}
+
+putchar("\n");
+
+return (0);
+}
