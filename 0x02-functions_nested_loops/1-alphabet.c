8a2eeb037b80d8b01015ad6be514fc21940dc811
Felectro9 <sramerafe1@gmail.com>
2022-09-20 09:02:59 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * print alphabet-uses putchar function to a - z
+ *
+ */
+
+void print_alphabet(void)
+{
+	int alp;
+	
+	for (alp = 'a'; alp <= 'z'; ++alp)
+		_putchar(ch);
+	_putchar('\n');
+}	
