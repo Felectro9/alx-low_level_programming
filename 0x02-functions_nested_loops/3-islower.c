6a66accac0e49417de8eb465c1cf0cbe4843d4f7
Felectro9 <sramerafe1@gmail.com>
2022-09-22 13:16:02 -0700

    CHECKS FOR LOWERCASE

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower: to check if character is lowercase
  *
- *Description: @c to check input 
+ *Description:@c to check input 
  *
  * Return: returns 1 if c is lowercase otherwise always 0 (Success)
  */
