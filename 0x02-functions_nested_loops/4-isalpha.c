fc6ebc08e7ae3f644f85f616a4c68cb61941a27c
Felectro9 <sramerafe1@gmail.com>
2022-09-22 13:35:25 -0700

    CHECKS FOR UPPER AND LOWERCASE

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _isalpha - checks if character is both lower and uppercase
+ * @c : takes input from other functions
+ * Return: 1 if c is true else 0
+ */
+int isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
