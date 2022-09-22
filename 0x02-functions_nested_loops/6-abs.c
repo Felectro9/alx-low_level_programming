f4d108bb776024f67f9c97e07c3be01e67eba439
Felectro9 <sramerafe1@gmail.com>
2022-09-22 14:43:59 -0700

    ABSOLUTE VALUES

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _abs - computes absolute value of an integer
+ * Description: @n takes integer input for function
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
