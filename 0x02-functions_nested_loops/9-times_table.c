2c4edd642963f717dd4b78432275a3ac62f8e337
Felectro9 <sramerafe1@gmail.com>
2022-09-22 15:52:51 -0700

    NINE TIMES TABLE

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * times_table - outputs the nine times table
+ */
+void times_table(void)
+{
+	int n, times, result;
+
+	for (n = 0; n <= 9; ++n)
+	{
+		_putchar(48);
+		for (times = 1; times <= 9; ++times)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = n * times;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				putchar((result / 10) + 48);
+
+			putchar((result % 10) + 48)
+		}
+		putchar('\n');
+
+	}
+}
+				
