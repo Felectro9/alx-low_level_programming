9bd49336befee251d60fa4b932935811929ee645
Felectro9 <sramerafe1@gmail.com>
2022-09-24 09:50:22 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+/*
+ *               and updates the value it points to 98
+ *
+ *                *
+ *
+ *                 * @n: int parameter
+ *
+ *                  *
+ *
+ *                   * Return: Nothing
+ *
+ *                    */
+ void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+
+}
