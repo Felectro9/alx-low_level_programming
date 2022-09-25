ce303cddf32351182745a2cd574d881cb22896de
Felectro9 <sramerafe1@gmail.com>
2022-09-24 23:27:34 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,9 @@
 #include<unistd.h>
-
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: print a quote using write function
- *
- *     *    ssize_t write(int fd, const void *buf, size_t count);
- *
- *      *
- *
- *       * Return: This time we return an error 1.
- *
- *       */
-
-
-
+ * main - Entry point
+ * Description: print a quote using write function
+ * Return: This time we return an error 1.
+ */
 int main(void)
 
 {
