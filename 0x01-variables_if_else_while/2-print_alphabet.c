769969ce1ef7308a3dfdfd999e17214069668daa
Felectro9 <sramerafe1@gmail.com>
2022-09-24 23:55:23 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,13 @@
 # include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Description - print lower case alphabest
- *
  * return 0 (success)
- * 
  */
-
 int main(void)
 {
 	char ch = 'a';
-        
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
