835a62f15f98b81b731bd8f0c3452fed1fe5f7ac
Felectro9 <sramerafe1@gmail.com>
2022-10-15 02:51:09 -0700

    STRING FILE

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * Description - function to locate a character in a string
+ * @s: pointer to our string array input
+ * @c: character to locate from input array
+ * Return: character or null if not found
+ */
+char *_strchr(char *s, char c);
+{
+	while (*s != '\0')
+	{
+		if(*s == c)
+			return(s);
+		s++;
+	}
+	if (*s == c)
+		return(s);
+	return('\0');
+}
